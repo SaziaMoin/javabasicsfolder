6dd758ec0fb12ec84eec5ff00dbd4479c3dea196
Sazia <sazianagma@gmail.com>
2023-01-27 21:26:05 +0530

    modified commit

--- arithmaticoperator.java
+++ arithmaticoperator.java
@@ -2,8 +2,7 @@ public class arithmaticoperator
 {
     public static void main(String args[])
     {
-        int a=4,b=9;
-
+        int a=4,b=9,c=10;
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
